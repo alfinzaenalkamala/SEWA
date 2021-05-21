a329bedf6a958e0f7689bad5e4b2827bd6645631
alfinzaenalkamala <alfinzaenalkamala@gmail.com>
2021-05-21 22:18:54 +0700

    splashScrean.kt dan MAinactivity icon belom

---
+++ app/src/main/java/com/dpag/sewa/HalamanFaforit.kt
@@ -0,0 +1,11 @@
+package com.dpag.sewa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanFaforit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_faforit)
+    }
+}
---
+++ app/src/main/java/com/dpag/sewa/HalamanTerdekat.kt
@@ -0,0 +1,11 @@
+package com.dpag.sewa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanTerdekat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_terdekat)
+    }
+}
