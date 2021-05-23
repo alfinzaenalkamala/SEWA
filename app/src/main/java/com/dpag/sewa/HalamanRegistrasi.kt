7a8f1c1dcf5ca9f6bb5512ec7620949540fbdd21
alfinzaenalkamala <alfinzaenalkamala@gmail.com>
2021-05-23 14:45:45 +0700

    login register

---
+++ app/src/main/java/com/dpag/sewa/HalamanRegistrasi.kt
@@ -0,0 +1,11 @@
+package com.dpag.sewa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanRegistrasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_registrasi)
+    }
+}
