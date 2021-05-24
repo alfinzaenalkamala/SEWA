6fcecd094e5a0cff71e2c5e573b16f39d34d6711
alfinzaenalkamala <alfinzaenalkamala@gmail.com>
2021-05-24 11:01:45 +0700

    inten 2

--- app/src/main/java/com/dpag/sewa/Detail.kt
+++ app/src/main/java/com/dpag/sewa/Detail.kt
@@ -10,11 +10,6 @@ class Detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
